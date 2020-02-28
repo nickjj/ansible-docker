6cc1149b1536d1b4894d3d9d6a4fdd9c8a07d248
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-02-28 21:28:08 +0100

    Move test script to repo

---
+++ tests/test.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+#
+# Ansible role test shim.
+#
+# Taken from this gist by Jeff Geerling on May 10th 2018:
+#   https://gist.github.com/geerlingguy/73ef1e5ee45d8694570f334be385e181
+#
+# Usage: [OPTIONS] ./tests/test.sh
+#   - distro: a supported Docker distro version (default = "ubuntu1804")
+#   - playbook: a playbook in the tests directory (default = "test.yml")
+#   - role_dir: the directory where the role exists (default = $PWD)
+#   - cleanup: whether to remove the Docker container (default = true)
+#   - container_id: the --name to set for the container (default = timestamp)
+#   - test_idempotence: whether to test playbook's idempotence (default = true)
+#
+# If you place a requirements.yml file in tests/requirements.yml, the
+# requirements listed inside that file will be installed via Ansible Galaxy
+# prior to running tests.
+#
+# License: MIT
+
+# Exit on any individual command failure.
+set -eE
+
+# Remove the Docker container (if configured).
+cleanup() {
+  if [ "$cleanup" = true ]; then
+    printf "Removing Docker container...\n"
+    docker rm -f $container_id
+  fi
+}
+
+trap "cleanup" ERR
+
+# Pretty colors.
+red='\033[0;31m'
+green='\033[0;32m'
+neutral='\033[0m'
+
+timestamp=$(date +%s)
+
+# Allow environment variables to override defaults.
+distro=${distro:-"ubuntu1804"}
+playbook=${playbook:-"test.yml"}
+role_dir=${role_dir:-"$PWD"}
+cleanup=${cleanup:-"true"}
+container_id=${container_id:-$timestamp}
+test_idempotence=${test_idempotence:-"true"}
+
+## Set up vars for Docker setup.
+# CentOS 7
+if [ $distro = 'centos7' ]; then
+  init="/usr/lib/systemd/systemd"
+  opts="--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+# CentOS 6
+elif [ $distro = 'centos6' ]; then
+  init="/sbin/init"
+  opts="--privileged"
+# Ubuntu 18.04
+elif [ $distro = 'ubuntu1804' ]; then
+  init="/lib/systemd/systemd"
+  opts="--privileged --volume=/var/lib/docker --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+# Ubuntu 16.04
+elif [ $distro = 'ubuntu1604' ]; then
+  init="/lib/systemd/systemd"
+  opts="--privileged --volume=/var/lib/docker --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+# Ubuntu 14.04
+elif [ $distro = 'ubuntu1404' ]; then
+  init="/sbin/init"
+  opts="--privileged --volume=/var/lib/docker"
+# Ubuntu 12.04
+elif [ $distro = 'ubuntu1204' ]; then
+  init="/sbin/init"
+  opts="--privileged --volume=/var/lib/docker"
+# Debian 10
+elif [ $distro = 'debian10' ]; then
+  init="/lib/systemd/systemd"
+  opts="--privileged --volume=/var/lib/docker --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+# Debian 9
+elif [ $distro = 'debian9' ]; then
+  init="/lib/systemd/systemd"
+  opts="--privileged --volume=/var/lib/docker --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+# Debian 8
+elif [ $distro = 'debian8' ]; then
+  init="/lib/systemd/systemd"
+  opts="--privileged --volume=/var/lib/docker --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+# Fedora 24
+elif [ $distro = 'fedora24' ]; then
+  init="/usr/lib/systemd/systemd"
+  opts="--privileged --volume=/var/lib/docker --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+# Fedora 27
+elif [ $distro = 'fedora27' ]; then
+  init="/usr/lib/systemd/systemd"
+  opts="--privileged --volume=/var/lib/docker --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+fi
+
+# Run the container using the supplied OS.
+printf ${green}"Starting Docker container: geerlingguy/docker-$distro-ansible."${neutral}"\n"
+docker pull geerlingguy/docker-$distro-ansible:latest
+docker run --detach -e USER="${USER}" --volume="$role_dir":/etc/ansible/roles/role_under_test:rw --name $container_id $opts geerlingguy/docker-$distro-ansible:latest $init
+
+printf "\n"
+
+# Install requirements if `requirements.yml` is present.
+if [ -f "$role_dir/tests/requirements.yml" ]; then
+  printf ${green}"Requirements file detected; installing dependencies."${neutral}"\n"
+  docker exec --tty $container_id env TERM=xterm ansible-galaxy install -r /etc/ansible/roles/role_under_test/tests/requirements.yml
+fi
+
+printf "\n"
+
+# Test Ansible syntax.
+printf ${green}"Checking Ansible playbook syntax."${neutral}
+docker exec --tty $container_id env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook --syntax-check
+
+printf "\n"
+
+# Run Ansible playbook.
+printf ${green}"Running command: docker exec $container_id env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook"${neutral}
+docker exec $container_id env TERM=xterm env ANSIBLE_FORCE_COLOR=1 ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook
+
+if [ "$test_idempotence" = true ]; then
+  # Run Ansible playbook again (idempotence test).
+  printf ${green}"Running playbook again: idempotence test"${neutral}
+  idempotence=$(mktemp)
+  docker exec $container_id ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook | tee -a $idempotence
+  tail $idempotence \
+    | grep -q 'changed=0.*failed=0' \
+    && (printf ${green}'Idempotence test: pass'${neutral}"\n") \
+    || (printf ${red}'Idempotence test: fail'${neutral}"\n" && exit 1)
+fi
+
+cleanup
